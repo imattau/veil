5a59a6eba8d5890d01aa365b652d88ce2a905ec0
imattau <imatt.au@protonmail.com>
2026-02-08 22:12:36 +1100

    build: update android minSdk to 21 and enable multi-dex

--- apps/veil_android/android/app/build.gradle.kts
+++ apps/veil_android/android/app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        multiDexEnabled = true
     }
 
     buildTypes {
